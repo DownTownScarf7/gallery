5eb3566341afa565b3ead73cc74cc2be622b2962
DownTownScarf7 <vbence741@gmail.com>
2018-06-24 16:09:51 +0200

    server now auto detects pictures

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 'use strict';
 
 const express = require('express'),
+      fs = require('fs'),
       app = express(),
       PORT = 3000,
-      pictures = [
-        { path: './pics/asd.jpg', title: 'testTitle', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laborum. Quis, saepe.' },
-        { path: './pics/secret.jpg', title: 'testTitle2', desc: 'Minima dignissimos rem laudantium nam ex reprehenderit ducimus accusamus quo cum tempore quae perferendis placeat voluptate, provident modi.' },
-      ];
+      files = fs.readdirSync('./pics'),
+      pictures = [];
+
+files.forEach(elem => {
+  pictures.push({ path: `./pics/${elem}`, title: elem.substring(0, elem.indexOf('.')), desc: 'testDesc' });
+});
 
 app.use('/assets', express.static('assets'));
 app.use('/pics', express.static('pics'));
 app.use('/static', express.static('static'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  let id = req.query.id && req.query.id < pictures.length ? req.query.id : 0;
   res.render('home', {
     pictures,
-    id,
   });
 });
 
